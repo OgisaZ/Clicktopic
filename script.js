8073883fbcf9ad131d9603a7f281fbc46a7e9715
OgisaZ <ogi.zurkic00@gmail.com>
2023-02-20 15:02:53 +0100

    Risk of rainify
    -Now gold instead of cookies
    -Survivor instead of factories
    -Added chests.
    When opening chests, you will get a random item (currently do nothing)
    The items will all do something (and hopefully stack inf)

--- script.js
+++ script.js
@@ -1,81 +1,127 @@
 'use strict';
 
 //Labels
-const labelCookiesNumber = document.querySelector(`.cookies`);
+const labelgoldNumber = document.querySelector(`.gold`);
 const labelHistoryText = document.querySelector(`.history-text`);
-
+const labelItemList = document.getElementById(`items-list`);
 //Inputs
 const inputFarmName = document.getElementById(`farm-name`);
 
 //Buttons
-const btnCookie = document.querySelector(`.cookie-button`);
+const btngold = document.querySelector(`.gold-button`);
 const btnFarmName = document.getElementById(`farm-name-button`);
+const btnChests = document.querySelector(`.chests`);
 //Property Buttons
 const btnClickMultiplier = document.getElementById(`clickMultiplier`);
-const btnFactory = document.getElementById(`factory`);
-
+const btnsurvivor = document.getElementById(`survivor`);
 //Get cash from localStorage if exists (|| if it doesn't)
-let cookies = Number(localStorage?.getItem(`cookies`)) || 0;
+let gold = Number(localStorage?.getItem(`gold`)) || 0;
 
-//How many cookies you get per click
-let cookiesPerClick = 1;
+const inventory = JSON.parse(localStorage.getItem(`inventory`)) || [];
+const inventoryUnique = new Set(inventory);
 
+const items = [
+  `Soldier's Syringe`,
+  `Lens Maker`,
+  `Tri-tip Dagger`,
+  `Roll of Pennies`,
+  `Crowbar`,
+];
+const counts =
+  JSON.parse(localStorage.getItem(`counts`)) || new Array(items.length).fill(0);
+const enemies = [`Lesser Wisp`, `Beetle`, `Lemurian`, `Stone Golem`];
+let enemyHealth;
+let randomNumber;
+const randomItemNumber = () => Math.trunc(Math.random() * items.length);
 //Get the object from localStorage if exists (|| if it doesn't)
 //property:[cost,how many you have, how much this property has made]
 const properties = JSON.parse(localStorage.getItem(`properties`)) || {
-  factory: [20, 0, 0],
+  survivor: [20, 0, 0],
   clickMultiplier: [40, 1, 0],
+  chests: [10, 0, 0],
 };
 
 //Have only one decimal place
-labelCookiesNumber.textContent = cookies.toFixed(1);
+labelgoldNumber.textContent = gold.toFixed(1);
 
-labelCookiesNumber.textContent = ``;
-// localStorage.clear(`cookies`);
+String.prototype.replaceAt = function (index, replacement) {
+  if (index >= this.length) {
+    return this.valueOf();
+  }
 
-function updateHistoryText(property) {
+  return this.substring(0, index) + replacement + this.substring(index + 1);
+};
+
+function enemyPicker() {
+  randomNumber = Math.trunc(Math.random() * enemies.length);
+  enemyHealth = (randomNumber + 1) * 5;
+  const randomEnemy = enemies[randomNumber];
+  btngold.value = `${randomEnemy} \n`;
+}
+function updateHistoryText(string) {
   labelHistoryText.style.opacity = 1;
   labelHistoryText.style.transition = `1s`;
-  labelHistoryText.textContent = `Bought ${property}!`;
-  setTimeout(e => {
-    labelHistoryText.style.opacity = 0;
-    labelHistoryText.style.transition = `1s`;
-  }, 1000);
+  labelHistoryText.textContent = `Got ${string}!`;
+  if (labelHistoryText.style.opacity <= 1) {
+    setTimeout(e => {
+      labelHistoryText.style.opacity = 0;
+      labelHistoryText.style.transition = `1s`;
+    }, 500);
+  }
 }
 
-function updateCookieCount() {
+function updategoldCount() {
   //Every 33 milliseconds (around 30 frames per second)
   setInterval(function () {
-    //Add the current cookies to localStorage
-    localStorage.setItem(`cookies`, cookies);
+    //Add the current gold to localStorage
+    localStorage.setItem(`gold`, gold);
 
     //Set the text to be fixed to have 1 decimal place
-    labelCookiesNumber.textContent = cookies.toFixed(1);
+    labelgoldNumber.textContent = gold.toFixed(1);
 
     //Add the current properties object to localStorage
     localStorage.setItem(`properties`, JSON.stringify(properties));
 
-    //And add cookies for each property per 33 milsec
-    cookies = cookies + properties.factory[1] * 0.001;
+    //And add gold for each property per 33 milsec
+    gold = gold + properties.survivor[1] * 0.001;
+    enemyHealth = enemyHealth - properties.survivor[1] * 0.001;
+    if (enemyHealth <= 0) {
+      gold = gold + (randomNumber + 1) * 2.5;
+      updateHistoryText((randomNumber + 1) * 2.5);
+      enemyPicker();
+    } else {
+      btngold.value = `${enemies[randomNumber]}\nHealth:${enemyHealth.toFixed(
+        1
+      )}`;
+    }
   }, 33);
 
   //Get the farm name from localStorage
   inputFarmName.value = localStorage.getItem(`farmName`);
 }
 function updateButtonValues() {
-  //Update factory numbers
-  btnFactory.value = `Buy factory(${properties.factory[1]}) \n ${properties.factory[0]}`;
+  //Update survivor numbers
+  btnsurvivor.value = `Buy survivor(${properties.survivor[1].toFixed(
+    1
+  )}) \n ${properties.survivor[0].toFixed(1)}`;
 
-  //Update cookie multiplier numbers
+  //Update gold multiplier numbers
   btnClickMultiplier.value = `Buy click multiplier(${properties.clickMultiplier[1].toFixed(
     1
-  )}) \n ${properties.clickMultiplier[0]}`;
+  )}) \n ${properties.clickMultiplier[0].toFixed(1)}`;
+
+  //Update chest numbers
+  btnChests.value = `Buy chest\n ${properties.chests[0].toFixed(1)}`;
+
+  labelItemList.textContent = localStorage.getItem(`inventoryText`);
 }
 
-//Call them instantly
-
+// localStorage.clear(`gold`);
+// Call them instantly
+enemyPicker();
 updateButtonValues();
-updateCookieCount();
+updategoldCount();
+
 function updatePrice(property) {
   //Locate the current price of property
 
@@ -89,35 +135,58 @@ function updatePrice(property) {
 
   //This is pretty smart im proud of myself
   //Change the text on the buttons, to add how many you have, and the price to buy another one
-  document.getElementById(`${property}`).value = `Buy ${property}(${properties[
-    property
-  ][1].toFixed(1)})\n ${price.toFixed(1)}`;
 }
 
-//Clicking the button, adds a cookie
-btnCookie.addEventListener(`click`, function () {
-  cookies = cookies + properties.clickMultiplier[1];
+function numberOfItems(item) {
+  const whereIsItem = items.findIndex(mov => mov === item);
+  counts[whereIsItem]++;
+  if (inventory.includes(item)) {
+    let whatNumberYouCurrentlyHave = Number(
+      labelItemList.textContent[labelItemList.textContent.indexOf(item) - 3]
+    );
+    let whereThatItemIsLocated = labelItemList.textContent.indexOf(item) - 3;
+    labelItemList.textContent = labelItemList.textContent.replaceAt(
+      whereThatItemIsLocated,
+      whatNumberYouCurrentlyHave + 1
+    );
+  } else {
+    labelItemList.textContent += `${
+      inventory.filter(mov => mov === `${item}`).length + 1
+    }x ${item},`;
+  }
+
+  return Number(counts[whereIsItem]);
+}
+
+//Clicking the button, adds a gold
+btngold.addEventListener(`click`, function () {
+  gold = gold + properties.clickMultiplier[1];
+  enemyHealth =
+    enemyHealth.toFixed(1) - properties.clickMultiplier[1].toFixed(1);
 });
 
-//Buying the factory button
-btnFactory.addEventListener(`click`, function (e) {
+//Buying the survivor button
+btnsurvivor.addEventListener(`click`, function (e) {
   //Do you have enough cash?
-  if (cookies >= properties.factory[0]) {
+  if (gold >= properties.survivor[0]) {
     //"Spend" cash (cash-price)
-    cookies = cookies - properties.factory[0];
+    gold = gold - properties.survivor[0];
 
     //Add the property to the object
-    properties.factory[1] = properties.factory.at(1) + 1;
+    properties.survivor[1] = properties.survivor.at(1) + 1;
 
     //Oh boy
-    updatePrice(`factory`);
-    updateHistoryText(`Factory`);
+    updatePrice(`survivor`);
+    updateHistoryText(`survivor`);
+    btnsurvivor.value = `Buy survivor(${properties.survivor[1].toFixed(
+      1
+    )})\n ${properties.survivor[0].toFixed(1)}`;
   }
 });
 
 btnClickMultiplier.addEventListener(`click`, function () {
-  if (cookies >= properties.clickMultiplier[0]) {
-    cookies = cookies - properties.clickMultiplier[0];
+  if (gold >= properties.clickMultiplier[0]) {
+    gold = gold - properties.clickMultiplier[0];
 
     properties.clickMultiplier[1] = properties.clickMultiplier[1] + 0.2;
 
@@ -144,3 +213,20 @@ btnFarmName.addEventListener(`click`, function (e) {
 //Insted of pressing "Enter" key, click anywhere to lose focus, and it will save it
 inputFarmName.onblur = e =>
   localStorage.setItem(`farmName`, inputFarmName.value);
+
+btnChests.addEventListener(`click`, function (e) {
+  if (gold >= properties.chests[0]) {
+    gold = gold - properties.chests[0];
+    let randomItem = items[randomItemNumber()];
+    updatePrice(`chests`);
+    btnChests.value = `Buy chest\n ${properties.chests[0].toFixed(1)}`;
+    numberOfItems(randomItem);
+    inventory.push(randomItem);
+    inventoryUnique.add(randomItem);
+
+    localStorage.setItem(`inventory`, JSON.stringify(inventory));
+    localStorage.setItem(`counts`, JSON.stringify(counts));
+    localStorage.setItem(`inventoryText`, labelItemList.textContent);
+    updateHistoryText(randomItem);
+  }
+});
